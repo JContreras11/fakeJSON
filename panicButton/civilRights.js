44a490f9d346871bf56a0c7e11a6f7ace1ed6c0f
Jesus Contreras <jesuscontreras1996102@gmail.com>
2021-05-23 20:09:48 -0500

    update civil rights data

---
+++ panicButton/civilRights.js
@@ -0,0 +1,20 @@
+[
+    {
+      "id": 0,
+      "name": "¿Dónde acudir en casos de violencia?",
+      "image": "https://raw.githubusercontent.com/JContreras11/fakeJSON/main/panicButton/civil_1.png",
+      "uri": "https://www.cddperu.org/es/noticias?type=comunicado"
+    },
+    {
+      "id": 1,
+      "name": "Información legal",
+      "image": "https://raw.githubusercontent.com/JContreras11/fakeJSON/main/panicButton/civil_2.png",
+      "uri": "https://www.cddperu.org/es/noticias?type=comunicado"
+    },
+    {
+      "id": 2,
+      "name": "Terminología",
+      "image": "https://raw.githubusercontent.com/JContreras11/fakeJSON/main/panicButton/civil_3.png",
+      "uri": "https://www.cddperu.org/es/noticias?type=comunicado"
+    }
+  ]
